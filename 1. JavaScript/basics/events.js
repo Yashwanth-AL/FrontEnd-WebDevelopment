6995be8520ad08847fce6aa4fe522b7dc49bfcf1
Yashwanth-AL <yashwanthal2004@gmail.com>
2024-08-24 18:31:29 +0530

    added files

--- 1. JavaScript/basics/events.js
+++ 1. JavaScript/basics/events.js
@@ -4,11 +4,13 @@ let curMode = "light";
 btn.addEventListener("click", (evt) => {
   if(curMode === "light"){
     curMode = "dark"; 
-    document.querySelector("body").style.backgroundColor = "gray";
+    document.querySelector("body").style.backgroundColor = "black";
+    document.querySelector("h1").style.color = "white"; 
   }
   else {
     curMode = "light"; 
     document.querySelector("body").style.backgroundColor = "white";
+    document.querySelector("h1").style.color = "black"; 
   }
   console.log(evt);
 });
